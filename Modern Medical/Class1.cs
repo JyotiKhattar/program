3794930f149d8c2499262bc3328c70f8462ce4af
JyotiKhattar <jyoti_khattar@hotmail.com>
2011-10-22 01:19:06 -0400

    commit

---
+++ Modern Medical/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modern_Medical
+{
+    class Class1
+    {
+    }
+}
---
+++ Modern Medical/ProgramNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modern_Medical
+{
+    class ProgramNumber
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Modern");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Medical");
+
+                }
+
+                else if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("ModernMedical");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
